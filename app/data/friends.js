f23531247e46f3dd791c7ffba91da11a54c60d8e
jasapper <jasap98@yahoo.com>
2018-01-18 00:32:13 -0500

    Setup bogus friends.js, api/htmlRoutes.js, start of UI dev

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+		name: "Scrooge McDuck",
+		photo: "http://thefederalist.com/wp-content/uploads/2015/06/scrooge-mcduck-998x749.jpg",
+		scores: [1, 5, 1, 1, 5, 5, 5, 1, 5, 5]
+	},{
+		name: "Peter Griffin",
+		photo: "https://cdn.costumewall.com/wp-content/uploads/2017/08/peter-griffin.jpg",
+		scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1]
+	},{
+		name: "Philip Fry",
+		photo: "http://images2.fanpop.com/image/photos/9400000/Fry-of-Futurama-philip-j-fry-9424597-352-458.gif",
+		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},{
+		name: "Homer Simpson",
+		photo: "https://static.scholar.harvard.edu/files/styles/profile_full/public/homer-simpson_36.jpg",
+		scores: [5, 1, 5, 2, 1, 1, 1, 5, 1, 1]
+	}];
+
+module.exports = friends;
